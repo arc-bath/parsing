bed21da01c3a796d6edce51a711b0bdf2e0d3e28
James Grant <r.j.grant@bath.ac.uk>
2018-12-04 14:37:52 +0000

    Added tests for ex 4

--- src/test_ts_proc.py
+++ src/test_ts_proc.py
@@ -2,7 +2,7 @@ import ts_parser as tsp
 
 def test_proc_ts_coords():
     '''
-    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    Test that proc_structs(filename) returns a list of 4 lists(of lists)
     '''
     filename = '../data/ts_coords.txt'
 
@@ -20,7 +20,7 @@ def test_proc_ts_coords():
 
 def test_proc_struct_a():
     '''
-    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    Test that proc_structs(filename) returns a list of 4 lists(of lists)
     '''
     filename = '../data/test_structs_a.txt'
 
@@ -36,7 +36,7 @@ def test_proc_struct_a():
 
 def test_proc_struct_b():
     '''
-    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    Test that proc_structs(filename) returns a list of 4 lists(of lists)
     '''
     filename = '../data/test_structs_b.txt'
 
@@ -52,7 +52,7 @@ def test_proc_struct_b():
 
 def test_proc_struct_c():
     '''
-    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    Test that proc_structs(filename) returns a list of 4 lists(of lists)
     '''
     filename = '../data/test_structs_c.txt'
 
@@ -68,7 +68,7 @@ def test_proc_struct_c():
 
 def test_proc_struct_d():
     '''
-    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    Test that proc_structs(filename) returns a list of 4 lists(of lists)
     '''
     filename = '../data/test_structs_d.txt'
 
@@ -82,9 +82,9 @@ def test_proc_struct_d():
     assert struct_info[0] == num_structs
     assert struct_info[1] == num_atoms
 
-def test_proc_struct_3():
+def test_proc_struct_e():
     '''
-    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    Test that proc_structs(filename) returns a list of 4 lists(of lists)
     '''
     filename = '../data/test_structs_e.txt'
 
@@ -97,5 +97,102 @@ def test_proc_struct_3():
 
     assert struct_info[0] == num_structs
     assert struct_info[1] == num_atoms
+
+
+def test_proc_ts_coords():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    '''
+    filename = '../data/ts_coords.txt'
+
+    structs = tsp.read_structs(filename)
+
+    struct_info = tsp.proc_structs(structs)
+
+    num_inv = 0
+    list_inv = []
+
+    assert struct_info[2][0] == num_inv
+    assert struct_info[2][1] == list_inv
+
+def test_proc_struct_a():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    '''
+    filename = '../data/test_structs_a.txt'
+
+    structs = tsp.read_structs(filename)
+
+    struct_info = tsp.proc_structs(structs)
+
+    num_inv = 0
+    list_inv = []
+
+    assert struct_info[2][0] == num_inv
+    assert struct_info[2][1] == list_inv
+
+def test_proc_struct_b():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    '''
+    filename = '../data/test_structs_b.txt'
+
+    structs = tsp.read_structs(filename)
+
+    struct_info = tsp.proc_structs(structs)
+
+    num_inv = 0
+    list_inv = []
+
+    assert struct_info[2][0] == num_inv
+    assert struct_info[2][1] == list_inv
+
+def test_proc_struct_c():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    '''
+    filename = '../data/test_structs_c.txt'
+
+    structs = tsp.read_structs(filename)
+
+    struct_info = tsp.proc_structs(structs)
+
+    num_inv = 0
+    list_inv = []
+
+    assert struct_info[2][0] == num_inv
+    assert struct_info[2][1] == list_inv
+
+def test_proc_struct_d():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    '''
+    filename = '../data/test_structs_d.txt'
+
+    structs = tsp.read_structs(filename)
+
+    struct_info = tsp.proc_structs(structs)
+
+    num_inv = 1
+    list_inv = [ 1 ]
+
+    assert struct_info[2][0] == num_inv
+    assert struct_info[2][1] == list_inv
+
+def test_proc_struct_e():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists(of lists)
+    '''
+    filename = '../data/test_structs_e.txt'
+
+    structs = tsp.read_structs(filename)
+
+    struct_info = tsp.proc_structs(structs)
+
+    num_inv = 1
+    list_inv = [ 1 ]
+
+    assert struct_info[2][0] == num_inv
+    assert struct_info[2][1] == list_inv
 
 
