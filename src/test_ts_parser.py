f50b478a5c3f1d06168f922576d95290415f1e13
James Grant <r.j.grant@bath.ac.uk>
2018-12-04 10:29:43 +0000

    Additional test structs and new tests

--- src/test_ts_parser.py
+++ src/test_ts_parser.py
@@ -1,4 +1,5 @@
 import ts_parser as tsp
+import numpy as np
 
 def test_read_ts_coords():
     '''
@@ -99,3 +100,86 @@ def test_read_structs_c():
         assert len(item[0]) == 2
         assert len(item[1]) == 4
         assert len(item[2]) == 6
+
+def test_read_structs_d():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists 
+    of the expected length
+    Completes unfinished atoms with `nan`
+    '''
+    expect = 4
+
+    filename = '../data/test_structs_d.txt'
+
+    structs = tsp.read_structs(filename)
+
+    assert len(structs) == expect
+
+    for item in structs:
+        assert len(item) == 3
+        assert len(item[0]) == 2
+        assert len(item[1]) == 4
+        assert len(item[2]) == 6
+
+    # Check that z[1][3] is `nan`
+    assert np.isnan( structs[3][1][3] )
+
+def test_read_structs_e():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists 
+    of the expected length
+    Completes unfinished atoms with `nan`
+    '''
+    expect = 4
+
+    filename = '../data/test_structs_e.txt'
+
+    structs = tsp.read_structs(filename)
+
+    assert len(structs) == expect
+
+    for item in structs:
+        assert len(item) == 3
+        assert len(item[0]) == 2
+        assert len(item[1]) == 4
+        assert len(item[2]) == 6
+
+    # Check that z[1][1:] are `nan`
+    for value in structs[3][1][1:]:
+        assert np.isnan( value )
+    # Check that y[1][2:] are `nan`
+    for value in structs[2][1][2:]:
+        assert np.isnan( value )
+    # Check that x[1][3] are `nan`
+    for value in structs[1][1][3:]:
+        assert np.isnan( value )
+
+def test_read_structs_f():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists 
+    of the expected length
+    Completes unfinished atoms with `nan`
+    '''
+
+    filename = '../data/test_structs_f.txt'
+
+    try:
+        structs = tsp.read_structs(filename)
+    except Exception as err:
+        assert isinstance(type(err), Exception)
+
+def test_read_structs_g():
+    '''
+    Test that read_structs(filename) returns a list of 4 lists 
+    of the expected length
+    Completes unfinished atoms with `nan`
+    '''
+
+    filename = '../data/test_structs_g.txt'
+
+    try:
+        structs = tsp.read_structs(filename)
+    except Exception as err:
+        assert isinstance(type(err), Exception)
+
+ 
